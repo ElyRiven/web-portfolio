a28a85965b4d8e49fbf157cc896eb15f1e53313c
Daniel Tamayo <byrdanto@gmail.com>
2024-01-23 20:35:36 -0500

    Updated the sidebar and changed the title icon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	darkMode: "class",
-	theme: {
-		extend: {
-			colors: {
-				"white-pl": "#fdfaf9",
-				"orange-pl": "#f79c6a",
-				"black-pl": "#131a21",
-				"pink-pl": "#db46d9",
-				"skyblue-pl": "#2e69f2",
-				"purple-pl": "#6030e0",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "white-pl": "#fdfaf9",
+        "orange-pl": "#f79c6a",
+        "black-pl": "#131a21",
+        "pink-pl": "#db46d9",
+        "skyblue-pl": "#2e69f2",
+        "purple-pl": "#6030e0",
+      },
+    },
+  },
+  plugins: [],
 };
